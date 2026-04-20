8d3b8bce43aa13d0d39b6cac74580f3deb3be268
Pawel <pawel.hiszpanski@gmail.com>
2026-04-20 21:52:52 +0200

    new functions added: EXIST, DBSIZE, KEYS, RENAME

--- redis_clone/main.cpp
+++ redis_clone/main.cpp
@@ -4,6 +4,8 @@
 #include <unordered_map>
 #include <fstream>
 
+using namespace std;
+
 void showTutorial() {
 	std::cout << "Tutorial for using Redis Clone\n";
 	std::cout << "1. To set a key-value pair, use command -> SET\n";
@@ -15,11 +17,17 @@ void showTutorial() {
 	std::cout << "4. To exit the program, use command -> EXIT\n";
 	std::cout << "5. To clear the view, use command -> CLEAR\n";
 	std::cout << "6. To clear whole database, use command -> NEW\n";
+	std::cout << "7. To check if key exist in database, use command -> EXISTS\n";
+	std::cout << "   Example: EXISTS key\n";
+	std::cout << "8. To size of database, use command -> DBSIZE\n";
+	std::cout << "9. To show all keys in database, use command -> KEYS\n";
+	std::cout << "10. To change key value, use command -> RENAME\n";
+	std::cout << "    Example: RENAME old_key new_key\n";
 }
 
 void saveToFile(std::unordered_map<std::string, std::string>& db) {
 	std::ofstream plik;
-	plik.open("database.txt");		// trunc for clearing file before saving
+	plik.open("database.txt");
 	if (plik.is_open()) {
 		for (auto& [key, val] : db) {
 			plik << key << " " << val << "\n";
@@ -115,9 +123,59 @@ void delHandler(std::unordered_map<std::string, std::string>& db, std::stringstr
 	saveToFile(db);
 }
 
-void exitHandler(std::unordered_map<std::string, std::string>& db, std::stringstream& ss, bool&end) {
+void exitHandler(std::unordered_map<std::string, std::string>& db, bool&end) {
 	end = true;
 	std::cout << "Closing Redis Clone! Thank you for using our services!\n";
+}
+
+void existHandler(std::unordered_map<std::string, std::string>& db , std::stringstream& ss) {
+	string key;
+	if (!(ss >> key)) {
+		std::cout << "ERROR -> key must be provided!\n";
+	}
+	else {
+		if (db.count(key)) {
+			cout << "Key exist\n";
+			return;
+		}
+		cout << "Key do NOT exist\n";
+	}
+}
+
+void dbsizeHandler(std::unordered_map<std::string, std::string>& db) {
+	cout << "Database hold " << db.size() << " elements\n";
+}
+
+void keysHandler(std::unordered_map<std::string, std::string>& db) {
+	int i = 1;
+	cout << "Keys in database:\n";
+	for (auto [key, val] : db) {
+		cout << i++ << ". " << key << "\n";
+	}
+	if (i == 1) std::cout << "-----\n";
+}
+
+void renameHandler(std::unordered_map<std::string, std::string>& db, std::stringstream& ss) {
+	std::string old_key, new_key;
+	if (!(ss >> old_key)) {
+		std::cout << "ERROR -> old key must be provided!\n";
+		return;
+	}
+	if (!(ss >> new_key)) {
+		std::cout << "ERROR -> new key must be provided!\n";
+		return;
+	}
+	auto it = db.find(old_key);
+	if (it == db.end()) {
+		std::cout << "ERROR -> provided key does NOT exist!";
+	}
+	else {
+		string tmp = db.at(it->first);		// copy of value of key 'it'
+		db.erase(it);
+		db[new_key] = tmp;
+		std::cout << "Key changed succesfully\n";
+		saveToFile(db);
+	}
 }
 
 int main() {
@@ -149,7 +207,7 @@ int main() {
 			system("cls");
 		}
 		else if (cmd == "EXIT" || cmd == "QUIT" || cmd =="Q") {
-			exitHandler(db, ss, end);
+			exitHandler(db, end);
 		}
 		else if (cmd == "SET") {
 			setHandler(db, ss);
@@ -162,6 +220,18 @@ int main() {
 		}
 		else if (cmd == "NEW") {
 			clearFile(db);
+		}
+		else if (cmd == "EXISTS") {
+			existHandler(db, ss);
+		}
+		else if (cmd == "DBSIZE") {
+			dbsizeHandler(db);
+		}
+		else if (cmd == "KEYS") {
+			keysHandler(db);
+		}
+		else if (cmd == "RENAME") {
+			renameHandler(db, ss);
 		}
 		else {
 			std::cout << "Command unknown, check tutorial by typing T\n";
